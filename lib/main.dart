08f231f9534d34d083fd3bbad1b744702fbd47f0
phamanhkhoa323 <93703554+phamanhkhoa323@users.noreply.github.com>
2021-12-22 00:46:37 +0700

    Them Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'package:app_doan/dk_dn/dangky.dart';
 import 'package:flutter/material.dart';
 import 'dk_dn/dangnhap.dart';
 //0306191323_PhamAnhKhoa
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/dangnhap': (context) => const DangNhap(),
+        '/dangky': (context) => const DangKy(),
+      },
+      initialRoute: '/dangnhap',
+    );
+  }
 }
