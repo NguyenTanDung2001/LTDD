b77da2195be99aa410dee50b95cd01760bff2466
nguvl190 <66902523+NguyenTanDung2001@users.noreply.github.com>
2021-12-21 23:07:52 +0700

    dangky.dart

---
+++ lib/dk_dn/dangky.dart
@@ -0,0 +1,156 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        // ignore: deprecated_member_use
+        RaisedButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              side: const BorderSide(color: Colors.red)),
+          onPressed: () {},
+          child: buildButtonColumn(color, Icons.arrow_back),
+        ),
+      ],
+    );
+
+    Widget textSection2 = const Padding(
+      padding: EdgeInsets.fromLTRB(10, 1, 1, 10),
+      child: Text(
+        'Registration',
+        style: TextStyle(
+          fontSize: 40,
+          fontFamily: 'MyFont',
+          color: Colors.red,
+        ),
+        softWrap: true,
+      ),
+    );
+
+    Widget username = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Enter your email...',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    Widget password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password...',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+    Widget enter_password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Enter Your Password...',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    Widget registerButton = RaisedButton(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          side: const BorderSide(color: Colors.red)),
+      child: const Text('Đăng Ký'),
+      textColor: Colors.white,
+      color: Theme.of(context).errorColor,
+      elevation: 10.0,
+      splashColor: Colors.blueGrey,
+      onPressed: () {
+        // Perform some action
+      },
+    );
+
+    Widget loginButton = RaisedButton(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          side: const BorderSide(color: Colors.red)),
+      child: const Text(''),
+      textColor: Colors.white,
+      color: Theme.of(context).focusColor,
+      elevation: 10.0,
+      splashColor: Colors.blueGrey,
+      onPressed: () {
+        // Perform some action
+      },
+    );
+
+    return MaterialApp(
+      title: 'Bài tập 1',
+      home: Scaffold(
+        backgroundColor: Colors.yellow[300],
+        body: ListView(
+          children: [
+            buttonSection,
+            Column(
+              children: [
+                Image.asset(
+                  'images/logoapp.png',
+                  width: 220,
+                  height: 220,
+                ),
+                textSection2,
+                const SizedBox(height: 10.0),
+                username,
+                const SizedBox(height: 10.0),
+                password,
+                const SizedBox(height: 10.0),
+                enter_password,
+                const SizedBox(height: 10.0),
+                registerButton,
+                const SizedBox(height: 10.0),
+                // const Text(
+                //   'Hoặc đăng nhập qua',
+                //   style: TextStyle(
+                //     fontWeight: FontWeight.bold,
+                //   ),
+                // ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column buildButtonColumn(
+    Color color,
+    IconData icon,
+  ) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(left: 10),
+        ),
+      ],
+    );
+  }
+}
