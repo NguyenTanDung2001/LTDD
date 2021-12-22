6b83c05436ccfdd6e7327498c9027168e95bfefd
QuangTuan <tuanpro99cuter@gmail.com>
2021-12-22 10:04:21 +0700

    thay doi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:app_doan/trang_chu/trangchu.dart';
 import 'package:flutter/material.dart';
 import 'dk_dn/dangnhap.dart';
+import 'trang_chu/trangchu.dart';
 //0306191323_PhamAnhKhoa
 
 void main() {
---
+++ lib/trang_chu/trangchu.dart
@@ -0,0 +1,2 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
