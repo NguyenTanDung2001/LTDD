1d1f58dc80b004ecd7aeb5f14a31828bfbce28cf
phamanhkhoa323 <93703554+phamanhkhoa323@users.noreply.github.com>
2022-01-03 10:41:25 +0700

    thay doi

--- lib/thanhtoan/thanhtoan.dart
+++ lib/thanhtoan/thanhtoan.dart
@@ -14,7 +14,19 @@ class ThanhToan extends StatelessWidget {
       home: Scaffold(
         //backgroundColor: Colors.yellow[300],
         appBar: homeAppBar(context),
-        bottomNavigationBar: BottomNavBar(),
+        bottomNavigationBar: Container(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 15),
+              primary: Colors.red,
+            ),
+            onPressed: () {},
+            child: const Text(
+              'Xác nhận mua hàng',
+              style: TextStyle(fontSize: 28),
+            ),
+          ),
+        ),
         body: ListView(
           children: <Widget>[
             const SizedBox(height: 15.0),
@@ -29,18 +41,8 @@ class ThanhToan extends StatelessWidget {
             ),
             const SizedBox(height: 7.0),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(1, 1, 10, 1),
-                  child: Text(
-                    '65 Huỳnh Thúc Kháng, P.Bến Nghé, Q.1, Tp.HCM',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
                 // ignore: deprecated_member_use
                 FlatButton(
                   shape: RoundedRectangleBorder(
@@ -59,6 +61,21 @@ class ThanhToan extends StatelessWidget {
                 ),
               ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 1, 0, 1),
+                  child: Text(
+                    '65 Huỳnh Thúc Kháng, P.Bến Nghé, Q.1, Tp.HCM',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
